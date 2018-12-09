e1e4e02988abcd77f5367f04b1b7d6c7ba58dc2f
unknown <calvin.l.fischer@gmail.com>
2018-12-09 02:54:35 -0700

    original commit

---
+++ cmake-build-debug/CMakeFiles/Suduko.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Suduko.dir/main.cpp.obj"
+  "Suduko.pdb"
+  "Suduko.exe"
+  "Suduko.exe.manifest"
+  "libSuduko.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Suduko.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
